47eca26165bb5683721d647fb02220113777a275
jimdevops19 <jimdevops@gmail.com>
2022-01-10 12:02:38 +0200

    Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+
+app = Flask(
+    __name__,
+    static_folder='static',
+    static_url_path='/static/',
+)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='127.0.0.1',
+        port=5000,
+        debug=True
+    )
