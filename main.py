429decc2e13f6ac246f4669bd0b02b61ca8c0dfb
jimdevops19 <jimdevops@gmail.com>
2022-01-10 12:06:42 +0200

    Commit

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from flask import Flask,render_template
 
 
-app = Flask(
-    __name__,
-    static_folder='static',
-    static_url_path='/static/',
-)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
